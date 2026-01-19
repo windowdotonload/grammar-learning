c0f0cb2a4f892df429628badd56609ce6c7dcbe0
chenrui <chenrui@iscas.ac.cn>
2026-01-19 11:14:11 +0800

    feat: update VitePress config and add build scripts in package.json

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "英语语法大全",
   description: "English Grammar Encyclopedia",
+  base: "./",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
